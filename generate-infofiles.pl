91e447a04990905296615fabc140c1756555acd2
Benjamin Reed <rangerrick@befunk.com>
2010-02-02 13:34:46 -0500

    working amarok2

--- generate-infofiles.pl
+++ generate-infofiles.pl
@@ -69,6 +69,7 @@ my $translate = [
 	'libgadu',
 	'libgdiplus',
 	'libknotificationitem',
+	'liblastfm',
 	'libmal',
 	'libmath',
 	'libmusicbrainz4',
@@ -205,7 +206,7 @@ my $version_lookup = {
 		'^soprano.*?(-dev|-shlibs)?$'         => [ '2.3.70',       '1'    ],
 		'^strigi-gui.*?(-dev|-shlibs)?$'      => [ '0.6.5',        '1'    ],
 		'^taglib-extras.*$'                   => [ '1.0.1',        '1'    ],
-		'^taglib(-shlibs)?$'                  => [ '1.6.1',        '2'    ],
+		'^taglib(-shlibs)?$'                  => [ '1.6.1',        '3'    ],
 		'^unsermake$'                         => [ '0.4',          '0.20060316.1' ],
 	},
 };
